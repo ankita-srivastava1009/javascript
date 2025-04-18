b6e5eeb928fdd2684be60a81ebacc7f2a09906f6
Ankita Srivastava <ankitasrivastava1009@gmail.com>
2025-04-18 19:51:54 +0000

    container config done

--- test/first.js
+++ test/first.js
@@ -1,1 +1,17 @@
-console.log("ankita")
+const accountId = 1234
+let accountEmail = "ankita@gmail.accountEmail"
+var accountPassword ="3456"
+accountCity = "jaipur"
+
+accountEmail="nidhi@g,ail.com"
+accountPassword="7865"
+accountCity="bangluru"
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*prefer not to use var
+because of issue in block scope and functional scope
+
+*/
